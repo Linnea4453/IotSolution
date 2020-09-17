e6517900ff49c488f4e333329b457766a7d97b27
Linnea Nilsson <linnea.nilsson23@learnet.se>
2020-09-17 11:48:35 +0200

    Add project files.

---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Net.Http.Headers;
+using Microsoft.Azure.Devices.Client;
+using SharedLibraries.Models;
+using SharedLibraries.Services;
+
+namespace ConsoleApp
+{
+    class Program 
+    {
+        private static readonly string _conn = "";   //"Här lägger du meddelande från azure, Connection string "
+        private static readonly DeviceClient deviceClient = DeviceClient
+            .CreateFromConnectionString("_conn", TransportType.Mqtt);
+
+
+        static void Main(string[] args)
+        {
+           DeviceService.SendMessageAsync(deviceClient).GetAwaiter();
+
+        }
+    }
+}
---
+++ SharedLibraries/Services/DeviceService.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Azure.Devices.Client;
+using Newtonsoft.Json;
+using SharedLibraries.Models;
+
+namespace SharedLibraries.Services
+{
+    public static class DeviceService
+    {
+        private static readonly Random rnd = new Random();
+
+        public static async Task SendMessageAsync(DeviceClient deviceClient)
+        {
+            while (true)
+            {
+
+                var data = new TemperatureModel
+                {
+                    Temperature = rnd.Next(20, 30),
+                    Humidity = rnd.Next(40, 50)
+
+                };
+                //JSON skriver ut såhär STANDARD som alla applikationer kan läsa, därför konverterar vi {temperature : 20, humidity 44 }
+                var json = JsonConvert.SerializeObject(data);
+
+                var payload = new Message(Encoding.UTF8.GetBytes(json));
+                await deviceClient.SendEventAsync(payload);
+
+                Console.WriteLine($"Message sent {json}");
+
+                await Task.Delay(10 * 1000);
+
+            }
+        }
+    }
+}
